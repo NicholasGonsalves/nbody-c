df6263b7d9f856969f1647b646f67269bf5b96ea
Nicholas Gonsalves <gonzo.nick123@gmail.com>
2024-08-20 19:49:01 +0100

    logic fix

--- nbody.c
+++ nbody.c
@@ -37,7 +37,7 @@ void update_acceleration_all_bodies(struct Body bodies[], int size) {
             float dy = bodies[j].y - bodies[i].y;
             float dist_sq = dx * dx + dy * dy;
             float force = G * bodies[j].mass / dist_sq;
-            float distance = pow(dist_sq, 0.5);
+            float distance = sqrt(dist_sq);
             bodies[i].ax += force * dx / distance;
             bodies[i].ay += force * dy / distance;
         }
@@ -45,7 +45,7 @@ void update_acceleration_all_bodies(struct Body bodies[], int size) {
 }
 
 void simulate(struct Body bodies[], int size) {
-    update_acceleration_all_bodies(bodies, 10);
+    update_acceleration_all_bodies(bodies, size);
     for (int i = 0; i < size; i++) {
         update_position(&bodies[i]);
         update_velocity(&bodies[i]);
@@ -97,7 +97,7 @@ void renderBodies(struct Body bodies[], int size) {
     glLoadIdentity(); // Load the identity matrix to reset transformations
     glColor3f(1.0f, 1.0f, 1.0f);
     glBegin(GL_POINTS);
-    float scaling_factor = 1e13f;
+    float scaling_factor = 3e11f;
     for (int i = 0; i < size; i++) {
         glVertex2f(bodies[i].x / scaling_factor, bodies[i].y / scaling_factor); // Scale down positions
     }
@@ -109,21 +109,21 @@ int main() {
     GLFWwindow* window = initOpenGL();
     if (!window) return -1;
 
-    const int n = 10;
+    const int n = 5;
 
-    struct Body sun = {0, 0, 1.989 * 10e30, 0, 0, 0, 0};
-    struct Body mercury = {57.9e9, 0, 3.285 * 10e23, 0, 47.87e3, 0, 0};
-    struct Body venus = {108.2e9, 0, 4.867 * 10e24, 0, 35.02e3, 0, 0};
-    struct Body earth = {149.6e9, 0, 5.972 * 10e24, 0, 29.78e3, 0, 0};
-    struct Body mars = {227.9e9, 0, 6.39 * 10e23, 0, 24.07e3, 0, 0};
-    struct Body jupiter = {778.5e9, 0, 1.898 * 10e27, 0, 13.07e3, 0, 0};
-    struct Body saturn = {1.434e12, 0, 5.683 * 10e26, 0, 9.68e3, 0, 0};
-    struct Body uranus = {2.871e12, 0, 8.681 * 10e25, 0, 6.80e3, 0, 0};
-    struct Body neptune = {4.495e12, 0, 1.024 * 10e26, 0, 5.43e3, 0, 0};
-    struct Body pluto = {5.906e12, 0, 1.309 * 10e22, 0, 4.74e3, 0, 0};
+    struct Body sun = {0, 0, 1.989e30, 0, 0, 0, 0};
+    struct Body mercury = {57.9e9, 0, 3.285e23, 0, 47.87e3, 0, 0};
+    struct Body venus = {108.2e9, 0, 4.867e24, 0, 35.02e3, 0, 0};
+    struct Body earth = {149.6e9, 0, 5.972e24, 0, 29.78e3, 0, 0};
+    struct Body mars = {227.9e9, 0, 6.39e23, 0, 24.07e3, 0, 0};
+    struct Body jupiter = {778.5e9, 0, 1.898e27, 0, 13.07e3, 0, 0};
+    struct Body saturn = {1.434e12, 0, 5.683e26, 0, 9.68e3, 0, 0};
+    struct Body uranus = {2.871e12, 0, 8.681e25, 0, 6.80e3, 0, 0};
+    struct Body neptune = {4.495e12, 0, 1.024e26, 0, 5.43e3, 0, 0};
+    struct Body pluto = {5.906e12, 0, 1.309e22, 0, 4.74e3, 0, 0};
 
-    struct Body bodies[n] = {sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto};
-    // struct Body bodies[n] = {sun, mercury, venus, earth, mars};
+    // struct Body bodies[n] = {sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto};
+    struct Body bodies[n] = {sun, mercury, venus, earth, mars};
 
     while (!glfwWindowShouldClose(window)) {
         simulate(bodies, n);
